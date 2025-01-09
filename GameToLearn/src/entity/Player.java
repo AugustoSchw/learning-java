7dd8d9ac87ac6436193f05996a175c80d615ddf9
Augusto Schweitzer <augustoschweitzer2@gmail.com>
2025-01-09 00:40:01 -0300

    added player move and sprite

--- GameToLearn/src/entity/Player.java
+++ GameToLearn/src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -14,32 +17,72 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
         speed = 3;
         x = 100;
         y = 100;
+        direction = "down";
     }
 
     public void update() {
         if (keyH.upPressed) {
+            direction = "up";
             y -= speed;
         }
         else if (keyH.downPressed) {
+            direction = "down";
             y += speed;
         }
         else if (keyH.leftPressed) {
+            direction = "left";
             x -= speed;
         }
         else if (keyH.rightPressed) {
+            direction = "right";
             x += speed;
+        }
+    }
+
+    public void getPlayerImage() {
+        try {
+
+            up1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleU1.png"));
+            up2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleU2.png"));
+            down1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleD1.png"));
+            down2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleD2.png"));
+            left1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleL1.png"));
+            left2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleL2.png"));
+            right1 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleR1.png"));
+            right2 = ImageIO.read(getClass().getClassLoader().getResourceAsStream("player/idleR2.png"));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void draw(Graphics2D g2) {
 
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.getTileSize(), gp.getTileSize());
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.getTileSize(), gp.getTileSize(), null);
     }
 }
--- GameToLearn/src/main/Main.java
+++ GameToLearn/src/main/Main.java
@@ -4,7 +4,20 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        GamePanel gamePanel = initialize();
+        System.out.println("test");
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false); // so you can't change the size of the screen
+        window.setTitle("Best Game Ever");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.pack(); // puts the window in the size I chose
+
+        window.setLocationRelativeTo(null); // shown at the center of screen
+        window.setVisible(true);
+
         gamePanel.startGameThread();
     }
 
